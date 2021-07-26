45e515d82fe0db3ef9552bd7cd510734bb7bf34b
Y^^TED <76407715+YEETEDWIN@users.noreply.github.com>
2021-07-26 15:21:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(  # Finally, pass this all along to distutils to do the heavy lifting.
-    name="exploit",
+    name="multiploit",
     description="Python multi-purpose package",
     author="YEETEDWIN",
     author_email="yeetedwin@protonmail.com",
-    url="https://yeetedwin.is-a.dev/exploit",
-    download_url="https://yeetedwin.is-a.dev/exploit/installing.html",
+    url="https://yeetedwin.is-a.dev/multiploit",
+    download_url="https://yeetedwin.is-a.dev/multiploit/installing.html",
     project_urls={
-        'Documentation': 'https://yeetedwin.gitbook.io/exploit/',
-        'Installation': 'https://yeetedwin.gitbook.io/exploit/',
-        'Source Code': 'https://github.com/YEETEDWIN/exploit',
-        'Bug Tracker': 'https://github.com/YEETEDWIN/exploit/issues'
+        'Documentation': 'https://yeetedwin.gitbook.io/multiploit/',
+        'Installation': 'https://yeetedwin.gitbook.io/multiploit/',
+        'Source Code': 'https://github.com/YEETEDWIN/multiploit',
+        'Bug Tracker': 'https://github.com/YEETEDWIN/multiploit/issues'
     },
     long_description="README.rst",
     long_description_content_type="text/x-rst",
@@ -19,7 +19,6 @@ setup(  # Finally, pass this all along to distutils to do the heavy lifting.
     platforms="any",
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Framework :: Matplotlib',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
